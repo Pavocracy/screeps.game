9b3da2743ca4fc1caea468115721fb6a9a179b11
Roxxum <adam@roxxum.tech>
2021-05-11 14:50:22 +1000

    testing better ways to check

--- main.js
+++ main.js
@@ -3,36 +3,38 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-    
-    var harvesters = [];
-    var upgraders = [];
-    var builders = [];
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
-            harvesters.push(creep);
             roleHarvester.run(creep);
         }
+        if(_sum(creep.memory.role == 'harvester') < 1) {
+            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'harvester'})
+        }
         if(creep.memory.role == 'upgrader') {
-            upgraders.push(creep);
             roleUpgrader.run(creep);
         }
+        if(_sum(creep.memory.role == 'upgrader') < 1) {
+            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'harvester'})
+        }
         if(creep.memory.role == 'builder') {
-            builders.push(creep);
             roleBuilder.run(creep);
         }
+        if(_sum(creep.memory.role == 'builder') < 1) {
+            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'harvester'})
+        }
+    };
+/*
+    if(._sum(harvesters.length < 1) {
+        Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});
     };
 
-    if(harvesters.length < 1) {
-        Game.spawns.spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});
+    if(global.upgraders.length < 1) {
+        Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'upgrader'});
     };
 
-    if(upgraders.length < 1) {
-        Game.spawns.spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'upgrader'});
-    };
-
-    if(builders.length < 1) {
-        Game.spawns.spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'builder'});
-    };
+    if(global.builders.length < 1) {
+        Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'builder'});
+    };*/
 };
