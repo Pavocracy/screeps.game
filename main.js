1343102b8f22ca9e123ab44c35b47ec38c8b1d1f
Roxxum <adam@roxxum.tech>
2021-05-11 15:54:36 +1000

    simplified role check

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-var getRole = require('get.role');
 
 module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
+        harvesters = 0
+        upgraders = 0
+        builders = 0
 
         if(creep.memory.role == 'harvester') {
+            harvesters++
             roleHarvester.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
+            upgraders++
             roleUpgrader.run(creep);
         }
         if(creep.memory.role == 'builder') {
+            builders++
             roleBuilder.run(creep);
         }
-    };
 
-    for(var role in Game.creeps.memory.role) {
-        getRole.run(role);
-        
-        if (role > 1) {
-            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: role});
+        if(harvesters < 1) {
+            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});    
+        }
+        if(upgraders < 1) {
+            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'upgrader'});    
+        }
+        if(builders < 1) {
+            Game.spawns.spawn1.spawnCreep([WORK, CARRY, MOVE], null, {role: 'builder'});    
         }
     };
-
-};
+}
