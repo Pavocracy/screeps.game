08448167d8163728a3b4e02676561a1131c103db
Roxxum <adam@roxxum.tech>
2021-05-11 10:39:08 +1000

    empty arrays

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
-module.exports.loop = function () {
+var harvesters = [];
+var upgraders = [];
+var builders = [];
 
-    var harvesters = [];
-    var upgraders = [];
-    var builders = [];
+module.exports.loop = function () {
     
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
