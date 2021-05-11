d110c387500f3817b55a2afddfffb88c88adc67b
Roxxum <adam@roxxum.tech>
2021-05-11 10:26:10 +1000

    testing non empty array

--- main.js
+++ main.js
@@ -2,29 +2,29 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
-module.exports.loop = function () {
+var harvesters = [test];
+var upgraders = [];
+var builders = [];
 
-    var harvesters = [];
-    var upgraders = [];
-    var builders = [];
+module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
-            harvesters.push(Game.creeps[i]);
+            harvesters.push(creep);
             roleHarvester.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
-            upgraders.push(Game.creeps[i]);
+            upgraders.push(creep);
             roleUpgrader.run(creep);
         }
         if(creep.memory.role == 'builder') {
-            builders.push(Game.creeps[i]);
+            builders.push(creep);
             roleBuilder.run(creep);
         }
     };
 
-    if(harvesters.length < 1) {
+    if(harvesters.length < 2) {
         Game.spawns.spawn1.createCreep([Game.WORK, Game.CARRY, Game.MOVE], null, {role: 'harvester'});
     };
 
