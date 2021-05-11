041925657b5ab9097d653c80ecd47cf8fa53a267
Roxxum <adam@roxxum.tech>
2021-05-11 18:16:17 +1000

    removed console spam

--- garbage.js
+++ garbage.js
@@ -1,6 +1,4 @@
 global.garbage = function() {
-    console.log('Running Garbage')
-
     for(var i in Memory.creeps) {
         if(!Game.creeps[i]) {
             delete Memory.creeps[i];
--- spawner.js
+++ spawner.js
@@ -1,6 +1,4 @@
 global.spawner = function() {
-    console.log('Running Spawner')
-    
     if(global.harvesters < 1) {
         Game.spawns.spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});    
     };
