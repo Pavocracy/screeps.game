a9e14dee1da5116de28074ae2d1ea26f9e4fc867
Roxxum <adam@roxxum.tech>
2021-08-22 21:48:19 +1000

    added extensions up to level 5

--- structures.js
+++ structures.js
@@ -1,15 +1,22 @@
 global.structures = function() {
     for(var name in Game.rooms) {
         var room = Game.rooms[name];
-        var spawnX = Game.spawns['Spawn1'].pos.x;
-        var spawnY = Game.spawns['Spawn1'].pos.y;
-        var sites = Game.spawns['Spawn1'].room.find(FIND_MY_CONSTRUCTION_SITES).length;
-        var extensions = Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_EXTENSION}}).length;
+        var spawner = room.find(FIND_MY_SPAWNS)[0];
+        var spawnX = Game.spawns[spawner].pos.x;
+        var spawnY = Game.spawns[spawner].pos.y;
+        var sites = Game.spawns[spawner].room.find(FIND_MY_CONSTRUCTION_SITES).length;
+        var extensions = Game.spawns[spawner].room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_EXTENSION}}).length;
         if(room.controller.level == 2 && extensions < 5 && sites < 1) {
             room.createConstructionSite((spawnX), (spawnY + (extensions + 1)), STRUCTURE_EXTENSION);
         }
         if(room.controller.level == 3 && extensions > 4 && extensions < 10 && sites < 1) {
             room.createConstructionSite((spawnX), (spawnY - (extensions - 4)), STRUCTURE_EXTENSION);
+        }
+        if(room.controller.level == 4 && extensions > 9 && extensions < 20 && sites < 1) {
+            room.createConstructionSite((spawnX + (extensions - 9)), (spawnY), STRUCTURE_EXTENSION);
+        }
+        if(room.controller.level == 5 && extensions > 19 && extensions < 30 && sites < 1) {
+            room.createConstructionSite((spawnX - (extensions - 19)), (spawnY), STRUCTURE_EXTENSION);
         }
     }
 }
