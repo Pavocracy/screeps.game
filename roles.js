c863802ec241197aefe7f892897ade37112129f4
Roxxum <adam@roxxum.tech>
2021-08-22 21:49:45 +1000

    added bigger creeps when spawning

--- roles.js
+++ roles.js
@@ -3,10 +3,6 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 global.roles = function() {
-    global.harvesters = _(Game.creeps).filter( { memory: { role: 'harvester' } } ).size();
-    global.upgraders = _(Game.creeps).filter( { memory: { role: 'upgrader' } } ).size();
-    global.builders = _(Game.creeps).filter( { memory: { role: 'builder' } } ).size();
-
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
--- spawner.js
+++ spawner.js
@@ -1,12 +1,40 @@
 global.spawner = function() {
-    if(global.harvesters < 2) {
-        Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});    
-    }
-    if(global.upgraders < 2) {
-        Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'upgrader'});    
-    }
-    if(global.builders < 1) {
-        Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'builder'});    
+    for(var name in Game.rooms) {
+        var room = Game.rooms[name];
+        var level = room.controller.level;
+        var spawner = room.find(FIND_MY_SPAWNS)[0];
+        var harvesters = room.find(FIND_MY_CREEPS).filter( { memory: { role: 'harvester' } } ).size();
+        var upgraders = room.find(FIND_MY_CREEPS).filter( { memory: { role: 'upgrader' } } ).size();
+        var builders = room.find(FIND_MY_CREEPS).filter( { memory: { role: 'builder' } } ).size();
+        var energy = room.energyCapacityAvailable;
+        var parts = {
+            200:[WORK, CARRY, MOVE],
+            250:[WORK, CARRY, MOVE, MOVE],
+            300:[WORK, CARRY, CARRY, MOVE, MOVE],
+            350:[WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
+            400:[WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
+            450:[WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
+            500:[WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
+            550:[WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
+            600:[WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+            650:[WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+            700:[WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+            750:[WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+            800:[WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE],
+            850:[WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE],
+            900:[WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
+            950:[WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE],
+            1000:[WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
+        };
+        if(harvesters < (level * 2)) {
+            Game.spawns.spawner.createCreep(parts[energy], null, {role: 'harvester'});    
+        }
+        if(upgraders < (level * 2)) {
+            Game.spawns.spawner.createCreep(parts[energy], null, {role: 'upgrader'});    
+        }
+        if(builders < (level * 1)) {
+            Game.spawns.spawner.createCreep(parts[energy], null, {role: 'builder'});    
+        }
     }
 }
 
